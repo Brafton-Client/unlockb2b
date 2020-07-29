69057a3c72ba6a4f72e444ba9f3dca47fc0457ec
Deryk w. King <deryk.king@brafton.com>
2020-07-29 10:32:00 -0400

    add required tracking codes, linkedin, google tag and hotjar

--- wp/wp-content/themes/unlockb2b/header.php
+++ wp/wp-content/themes/unlockb2b/header.php
@@ -86,10 +86,68 @@
 			</script>';
 		}
 		?>
+		<!-- linkedin --> 
+		<script type="text/javascript">
+
+_linkedin_partner_id = "2191577";
+
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+</script><script type="text/javascript">
+
+(function(){var s = document.getElementsByTagName("script")[0];
+
+var b = document.createElement("script");
+
+b.type = "text/javascript";b.async = true;
+
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+
+s.parentNode.insertBefore(b, s);})();
+
+</script>
+		<!-- Hotjar Tracking Code for https://unlockb2b.com/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1919757,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+<!-- Google Tag Manager -->
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+
+})(window,document,'script','dataLayer','GTM-MDL4PV3');</script>
+
+<!-- End Google Tag Manager -->
 	</head>
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+	<noscript>
 
+<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=2191577&fmt=gif" />
+
+</noscript><!-- Google Tag Manager (noscript) -->
+
+<noscript><iframe 
+
+height="0" width="0" style="display:none;visibility:hidden" data-src="https://www.googletagmanager.com/ns.html?id=GTM-MDL4PV3" class="lazyload" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDL4PV3"
+
+height="0" width="0" style="display:none;visibility:hidden"></noscript></iframe></noscript>
+
+<!-- End Google Tag Manager (noscript) -->
 		<div id="container">
 
 			<header class="header" itemscope itemtype="http://schema.org/WPHeader">
